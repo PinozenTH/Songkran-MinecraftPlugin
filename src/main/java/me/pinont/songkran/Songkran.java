3cb392fd84b2e84c5bd58eebbe0c1f038954e738
pinozenth <Pinozen.Thailand@gmail.com>
2023-04-12 00:20:58 +0700

    implement onto jetbrain space

--- src/main/java/me/pinont/songkran/Songkran.java
+++ src/main/java/me/pinont/songkran/Songkran.java
@@ -4,6 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Songkran extends JavaPlugin {
 
+    public static Songkran plugin;
+    public Songkran() {
+        plugin = this;
+    }
     @Override
     public void onEnable() {
         // Plugin startup logic
