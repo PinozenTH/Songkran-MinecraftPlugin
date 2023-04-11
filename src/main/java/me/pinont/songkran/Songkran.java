c1a84c1d8059cef986dfe11070c715a2ee40f827
pinozenth <Pinozen.Thailand@gmail.com>
2023-04-12 00:14:17 +0700

    init

---
+++ src/main/java/me/pinont/songkran/Songkran.java
@@ -0,0 +1,17 @@
+package me.pinont.songkran;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Songkran extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
